d629168e5506039d5318672707beebbd0805d2c6
Elad Hirsch <eladh@jfrog.com>
2019-07-27 20:33:04 +0300

    add tools

--- eplus/src/generate.sh
+++ eplus/src/generate.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+echo 'please supply pgp key'
+    exit 1
+fi
+
+gpg --armor --export $1 > key.pub
+gpg --armor --export-secret-keys  $1 > key.priv
+
 cert=$(<key.pub)
 private=$(<key.priv)
 cert="${cert//$'\n'/\\\\n}"
 private="${private//$'\n'/\\\\n}"
 
-rm distribution.json temp.json artifactory.json 2> /dev/null
-
-sed "s~\"PUBLIC_KEY_VALUE\"~\"$cert\"~" dist-template.json >> temp.json
+sed "s~\"PUBLIC_KEY_VALUE\"~\"$cert\"~" dist-template.json > temp.json
 sed "s~\"PRIVATE_KEY_VALUE\"~\"$private\"~" temp.json > distribution.json
-sed "s~\"PUBLIC_KEY_VALUE\"~\"$cert\"~" arti-template.json >> artifatory.json
+sed "s~\"PUBLIC_KEY_VALUE\"~\"$cert\"~" arti-template.json > artifatory.json
+
+rm temp.json
+echo 'look for distribution.json and artifatory.json files"
