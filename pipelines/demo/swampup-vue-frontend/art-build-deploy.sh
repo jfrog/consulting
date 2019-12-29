8280b3c0adc17a59141350200b3d5ab8a4c87b79
Q Wang <wangq@jfrog.com>
2019-12-29 15:48:47 +0800

    fix front end

--- pipelines/demo/swampup-vue-frontend/art-build-deploy.sh
+++ pipelines/demo/swampup-vue-frontend/art-build-deploy.sh
@@ -3,23 +3,26 @@
 
 usage() {
     echo "Deploy a vue-based build to Artifactory using npm"
-    echo "Usage: $1 artifactory address"
-    exit 1
+    echo "Usage: $1 artifactory address";
+    echo "Usage: $1$2 artifactory password";
+#    exit 1
 }
 
-if [ -z "$1" ]; then
+if [ -z "$1" ] || [ -z "$2"  ] ; then
     usage
 fi
 
-artiUrl="$1"
+
+artiUrl=$1
+artPassword=$2
 
 #setup npm locally
-curl -u admin:password http://${artiUrl}:80/artifactory/api/npm/auth > ~/.npmrc
-echo "email = youremail@email.com" >> ~/.npmrc
+curl -u admin:${artPassword} http://${artiUrl}:80/artifactory/api/npm/auth > ~/.npmrc
+echo "email = wq237wq@gmail.com" >> ~/.npmrc
 npm config set registry http://${artiUrl}/artifactory/api/npm/npm-libs-local/
 
 # remove existing artifact
-curl -uadmin:password -XDELETE http://${artiUrl}:80/artifactory/npm-libs-local/frontend/-/frontend-3.0.0.tgz
+#curl -uadmin:${artPassword} -XDELETE http://${artiUrl}:80/artifactory/npm-libs-local/frontend/-/frontend-3.0.0.tgz
 
 # build and publish
 npm i && npm run build &&  npm publish
